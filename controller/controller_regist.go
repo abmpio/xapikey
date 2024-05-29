81e49b7724f576108de3319e31810e8ae4c5086e
jeseeqing <jeseeqing@gmail.com>
2024-05-29 21:21:45 +0800

    litter changed

--- controller/controller_regist.go
+++ controller/controller_regist.go
@@ -4,24 +4,13 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/abmpio/irisx/controllerx"
-	"github.com/abmpio/mongodbr"
 	"github.com/abmpio/xapikey/options"
 
-	"github.com/abmpio/abmp/pkg/log"
 	webapp "github.com/abmpio/webserver/app"
 )
 
 func registControllers(webApp *webapp.Application) {
 	newApiKeyController().RegistRouter(webApp, appendRouterPrefixPath("/xapikey"))
-}
-
-func RegistModelController[T mongodbr.IEntity](webApp *webapp.Application, routerPath string) {
-	entityController := &controllerx.EntityController[T]{
-		RouterPath: routerPath,
-	}
-	log.Logger.Debug(fmt.Sprintf("正在构建路由,%s...", entityController.RouterPath))
-	entityController.RegistRouter(webApp)
 }
 
 func appendRouterPrefixPath(path string) string {
